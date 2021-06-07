efdabed79d1c290a0e458f9e22e090a7452865a6
mengxiangyu <mxiangyu@seas.upenn.edu>
2021-06-07 14:29:22 -0400

    Add in an automatically log-in function. If user has chosen to remember the password, it will be stored in cookie, next time, when he/she tries to access a page that needs log in, system will first check whether his information has stored in cookies, if is, log in automatically.

--- src/main/java/com/xiangyumeng/note/filter/LoginAccessFilter.java
+++ src/main/java/com/xiangyumeng/note/filter/LoginAccessFilter.java
@@ -6,6 +6,7 @@ import com.xiangyumeng.note.persistantObject.User;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -21,6 +22,25 @@ import java.io.IOException;
  *      2. static resources - staticResources, like js, css, images...
  *      3. let pass certain behaviors - operations use can do without logging in, like actionName = logIn...
  *      4. when user has logged in - check session, see whether there is user info, if not, redirect to login.jsp
+ *
+ *  automatic login, if user previously choose to remember him
+ *      via cookie,
+ *      when user is in log-out state and requesting pages that need log-in state, then request
+ *      for automatic log-in
+ *
+ *      purpose:
+ *          let user maintain a log-in state
+ *
+ *      realization:
+ *          from cookie object, get user name and password, automatically log in
+ *          1. get cookie array
+ *          2. see if its empty
+ *          3. if not, get cookie whose name is user
+ *          4. get the value - name and password, userName-userPwd
+ *          5. separate to be name and password - split("-")
+ *          6. request to log in  (actionName = "login" )
+ *          7. return
+ *
  */
 @WebFilter("/*")
 public class LoginAccessFilter implements Filter {
@@ -69,6 +89,42 @@ public class LoginAccessFilter implements Filter {
             filterChain.doFilter(request, response);
             return;
         }
+
+
+
+        // automatically log in, if there is a cookie
+        //from cookie object, get user name and password, automatically log in
+        //1. get cookie array
+        Cookie[] cookies = request.getCookies();
+
+        //2. see if its empty
+        if (cookies != null && cookies.length > 0){
+            //3. if not, get cookie whose name is user
+            for (Cookie cookie:cookies){
+                //4. get the value - name and password, userName-userPwd
+                if ("user".equals(cookie.getName())){
+                    String value = cookie.getValue();
+
+                    //5. separate to be name and password - split("-")
+                    String[] val = value.split("-");
+
+                    //6. request to log in  (actionName = "login" )
+                    String userName = val[0];
+                    String userPwd = val[1];
+
+                    String url = "user?actionName=login&userName=" + userName + "&userPwd=" + userPwd;
+                    request.getRequestDispatcher(url).forward(request,response);
+                    return;
+                }
+
+
+            }
+        }
+
+
+
+
+
 
 
         // none of the conditions meet, block the request, redirect to login page
